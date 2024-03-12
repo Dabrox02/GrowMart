4356e393d4f12f2599c83a110a1821112ea2adb7
Dabrox02 <jaiderstyvenmendoza@gmail.com>
2024-03-12 13:21:02 -0500

    feat: :sparkles: initial commit

---
+++ backend/src/main/java/com/campus/growmart/GrowmartApplication.java
@@ -0,0 +1,13 @@
+package com.campus.growmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowmartApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/campus/growmart/GrowmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campus.growmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
